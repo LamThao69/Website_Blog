68ea0d5deafb53d0edbad5fee9a25075ee89911b
TLABGitHub <tlabao563@gmail.com>
2025-09-17 08:19:59 +0700

    thêm database
    đã test thử login kết nối vs db thành công

---
+++ db.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "tlabsql5",
+  database: "webblog",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Kết nối thất bại:", err);
+  } else {
+    console.log("Kết nối thành công đến MySQL!");
+  }
+});
+
+module.exports = db;
