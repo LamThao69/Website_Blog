579b1ea588199ec57e178b25cafd124ce1c8cac6
Lâm Trung Thảo <lamtrungthao123@gmail.com>
2025-09-17 14:08:49 +0700

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "tlabsql5",
+  password: "310103",
   database: "webblog",
 });
 
